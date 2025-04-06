43b78401d781a27f92a37ff47c64a2abab6225d9
AdebimpeAbdulhamidEniola <adebimpeabdulhamid5@gmail.com>
2025-04-06 23:41:25 +0100

    Refactor code to improve functionality and performance

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import pool from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
+import { resourceUnavailable, generalErrorHandler } from "./middlewares/errorHandler.js";
+import createUserTable from "./data/createUserTable.js";
 
-dotenv.config();
+
+
+dotenv.config()
 
 const app = express();
 const port = process.env.PORT;
@@ -11,7 +17,32 @@ app.use(express.json());
 app.use(cors());
 
 
+//Routes
+//Testing database connection
+// app.get('/', async(req, res, next) => {
+//     try {
+//         const result = await pool.query("SELECT NOW()");
+//         console.log(result)
+//         res.send(result)
+//     }
+//     catch(err) {
+//         console.error(err)
+//     }
+// })
 
+//Create table before starting server and before going to an routes
+createUserTable();
+
+
+app.use('/users', userRoutes)
+
+
+
+
+
+// Error handling middleware
+app.use(resourceUnavailable);      //when user request didn't match any of the above endpoint
+app.use(generalErrorHandler) ;     //generalized error handling
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
