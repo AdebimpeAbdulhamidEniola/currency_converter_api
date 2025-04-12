4dec4c49d21dee98688900af330b188df84b254d
AdebimpeAbdulhamidEniola <adebimpeabdulhamid5@gmail.com>
2025-04-12 18:10:26 +0100

    Create model for inserting exchange rates into the datavase

---
+++ src/models/exchangeRateModel.js
@@ -0,0 +1,14 @@
+import pool from '../config/db.js';
+
+export const saveExchangeRate = async (base, target, rate) => {
+   try {
+    const query = `INSERT INTO exchange_rates (base_currency, target_currency, rate)
+     VALUES ($1, $2, $3) RETURNING *`
+     const rates = await pool.query(query, [base, target, rate]);  
+    return rates.rows[0];
+   }
+    catch (error) {
+     console.error('Error saving exchange rate:', error);
+     throw error;
+    }
+  };
