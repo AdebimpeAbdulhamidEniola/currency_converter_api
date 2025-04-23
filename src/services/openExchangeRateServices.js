6b6b63d69fa2f21aaa670304b172c0847496c738
AdebimpeAbdulhamidEniola <adebimpeabdulhamid5@gmail.com>
2025-04-23 21:57:54 +0100

    Refactor exchange rate service to export individual functions for latest and historical data retrieval

--- src/services/openExchangeRateServices.js
+++ src/services/openExchangeRateServices.js
@@ -5,9 +5,15 @@ dotenv.config()
 
 const OXR_APP_ID =  process.env.OXR_APP_ID
 
-export default async() => {
+export const latestExchange = async() => {
     const url = `https://openexchangerates.org/api/latest.json?app_id=${OXR_APP_ID}`
     const response = await axios.get(url)
     return response.data
 }
 
+//Get the data for a specific date
+export const historyData = async(date) => {
+    const historyUrl = `https://openexchangerates.org/api/historical/${date}.json?app_id=${OXR_APP_ID}`;
+    const historicalResponse = await axios.get(historyUrl);
+    return historicalResponse.data;
+}
