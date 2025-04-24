962606ced56b16fa509b5549b107d8af12b85d54
AdebimpeAbdulhamidEniola <adebimpeabdulhamid5@gmail.com>
2025-04-24 11:55:31 +0100

    Fix formatting and consistency in createUserTable function

--- src/data/createUserTable.js
+++ src/data/createUserTable.js
@@ -1,20 +1,20 @@
-import pool from "../config/db.js"
+import pool from "../config/db.js";
 
-const createUserTable = async() => {
-    const query =  `CREATE TABLE IF NOT EXISTS users(
-    id BIGSERIAL PRIMARY KEY NOT NULL,
-    email VARCHAR(100) UNIQUE NOT NULL,
-    password_hash VARCHAR(100) UNIQUE NOT NULL,
-    created_at TIMESTAMP DEFAULT NOW()
-)`
-try {
-    const newTable = await pool.query(query);
-    console.log("Table has successfully been created if not exist");
-}
-catch(error) {
-    console.log("Error creating users table", error);
-}
-
-}
+const createUserTable = async () => {
+    const query = `CREATE TABLE IF NOT EXISTS users (
+        id BIGSERIAL PRIMARY KEY NOT NULL,
+        email VARCHAR(100) UNIQUE NOT NULL,
+        password_hash VARCHAR(100) UNIQUE NOT NULL,
+        created_at TIMESTAMP DEFAULT NOW()
+    )`;
+    
+    try {
+        const newTable = await pool.query(query);
+        console.log("Table user has successfully been created if not exist");
+    }
+    catch (error) {
+        console.log("Error creating users table", error);
+    }
+};
 
 export default createUserTable;
