abfeef884a05dac82376156f923d40d3824d878a
Adebimpe Abdulhamid Eniola <142755049+AdebimpeAbdulhamidEniola@users.noreply.github.com>
2025-04-25 15:52:03 +0100

    Update redisClient.js in order to enable easy deployment

--- src/config/redisClient.js
+++ src/config/redisClient.js
@@ -1,6 +1,11 @@
 import {createClient} from "redis";
 
-const redisClient = createClient();
+const redisClient = createClient({
+    url: process.env.REDIS_URL,
+    socket: {
+        tls: true
+    }
+});
 
 redisClient.on("error", (error) => {
     console.error("Redis Client Error", error);
