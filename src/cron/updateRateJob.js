33c0e980ff703a0ffe51951590082cacf387d901
Adebimpe Abdulhamid Eniola <142755049+AdebimpeAbdulhamidEniola@users.noreply.github.com>
2025-04-29 07:15:33 +0100

    Update updateRateJob.js

--- src/cron/updateRateJob.js
+++ src/cron/updateRateJob.js
@@ -1,7 +1,7 @@
 import cron from "node-cron"
 import {getExchangeRates} from "../services/fetchAndStoreRates.js"
 
-cron.schedule("56 5 * * *", async () => {    
+cron.schedule("17 6 * * *", async () => {    
     try {
         await fetchAndStoreRates();
         console.log("Exchange rates updated successfully");
